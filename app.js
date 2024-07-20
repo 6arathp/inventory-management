379554c979de4e56b3489da8e696d992b9174f7f
6arathp <barathp.dev@gmail.com>
2024-07-20 14:48:20 +0530

    remove api dir and use express.static

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ dotenv.config()
 import cors from "cors"
 import * as db from './db.js'
 
+app.use(express.static("public"))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
